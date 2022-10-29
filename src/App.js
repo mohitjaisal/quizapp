2bd6b5c56ea2e85cfbda70aa61a1969add6f1f13
Ririio <xRiriio@gmail.com>
2022-10-28 22:50:43 -0400

    Automatically convers question amounts to max

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from "react";
+import { useGlobalContext } from "./context";
 
-import SetupForm from './SetupForm'
-import Loading from './Loading'
-import Modal from './Modal'
+import SetupForm from "./SetupForm";
+import Loading from "./Loading";
+import Modal from "./Modal";
 function App() {
   const {
     waiting,
@@ -13,52 +13,52 @@ function App() {
     correct,
     nextQuestion,
     checkAnswer,
-  } = useGlobalContext()
+  } = useGlobalContext();
   if (waiting) {
-    return <SetupForm />
+    return <SetupForm />;
   }
   if (loading) {
-    return <Loading />
+    return <Loading />;
   }
 
-  const { question, incorrect_answers, correct_answer } = questions[index]
-  
-  let answers = [...incorrect_answers]
-  const tempIndex = Math.floor(Math.random() * 4)
+  const { question, incorrect_answers, correct_answer } = questions[index];
+
+  let answers = [...incorrect_answers];
+  const tempIndex = Math.floor(Math.random() * 4);
   if (tempIndex === 3) {
-    answers.push(correct_answer)
+    answers.push(correct_answer);
   } else {
-    answers.push(answers[tempIndex])
-    answers[tempIndex] = correct_answer
+    answers.push(answers[tempIndex]);
+    answers[tempIndex] = correct_answer;
   }
   return (
     <main>
       <Modal />
-      <section className='quiz'>
-        <p className='correct-answers'>
+      <section className="quiz">
+        <p className="correct-answers">
           correct answers : {correct}/{index}
         </p>
-        <article className='container'>
+        <article className="container">
           <h2 dangerouslySetInnerHTML={{ __html: question }} />
-          <div className='btn-container'>
+          <div className="btn-container">
             {answers.map((answer, index) => {
               return (
                 <button
                   key={index}
-                  className='answer-btn'
+                  className="answer-btn"
                   onClick={() => checkAnswer(correct_answer === answer)}
                   dangerouslySetInnerHTML={{ __html: answer }}
                 />
-              )
+              );
             })}
           </div>
         </article>
-        <button className='next-question' onClick={nextQuestion}>
+        <button className="next-question" onClick={nextQuestion}>
           next question
         </button>
       </section>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
